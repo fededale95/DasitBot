a9e56143ad0d61264659a3e77c8712bff6a1b854
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-07-19 17:13:49 +0200

    afsafsa

--- index.js
+++ index.js
@@ -119,14 +119,14 @@ function zipme(dir_dms){
       var output = file_system.createWriteStream('/home/ubuntu/DMSEMA.zip');
       var archive = archiver('zip');
 
-      output.on('close', function () {
+      /*output.on('close', function () {
         console.log(archive.pointer() + ' total bytes');
         console.log('archiver has been finalized and the output file descriptor has closed.');
       });
 
       archive.on('error', function(err){
         throw err;
-      });
+      });*/
 
       archive.pipe(output);
 
