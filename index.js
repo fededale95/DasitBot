3ebd28b1bcaf6db0601c036051878060877803c6
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-07-28 10:16:52 +0200

    dsafcdsa

--- index.js
+++ index.js
@@ -72,9 +72,13 @@ function parseMessage( msg ){
                   if ( myArray[i].startsWith("0") || myArray[i].startsWith("1") || myArray[i].startsWith("2") || myArray[i].startsWith("3") || myArray[i].startsWith("4") || myArray[i].startsWith("5") || myArray[i].startsWith("6") || myArray[i].startsWith("7") || myArray[i].startsWith("8") || myArray[i].startsWith("9") ) {
 
                   } else {
-                     //myArray.splice(i, 1);
                      myArray[i]="CANC";
                   }
+            }
+            for(i in myArray){
+               if(myArray[i]=="CANC"){
+                  myArray.splice(i, 1);
+               }
             }
             //sendMes(msg.message.chat.id,"DMS CS EMA vers: "+myArray+" \nAttendi alcuni secondi, sto preparando il tuo download...");
             sendMes(msg.message.chat.id, ""+myArray);
