35df1364b74bafc6246409fe7ccd48a3cb9f8130
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-07-28 10:40:12 +0200

    dsds

--- index.js
+++ index.js
@@ -82,9 +82,10 @@ function parseMessage( msg ){
                   toCanc[i]=toCanc[i]-1;
                }
             }
-            myArray[myArray.length-1]=data.substring(0, data.length - 1);
+            last=myArray[myArray.length-1];
+            last2 = last.substring(0, last.length - 1);
             //sendMes(msg.message.chat.id,"DMS CS EMA vers: "+myArray[myArray.length-1]+" \nAttendi alcuni secondi, sto preparando il tuo download...");
-            sendMes(msg.message.chat.id, ""+myArray[myArray.length-1]);
+            sendMes(msg.message.chat.id, ""+last);
             directory_dms = '/mnt/nasCons/Nexus/DMSCSSperimentali/DMSEMA/'+data2;
             output_zip = '/home/dms/DMSEMA.zip';
             //zipDir(directory_dms, output_zip, msg.message.chat.id);
