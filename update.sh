7524da832a32185f7de6c21d28457654e5d76313
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-08-02 10:48:40 +0200

    ewgfedw

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 forever stopall
-rm index.js
-git clone https://github.com/fededale95/DasitBot.git
-mv DasitBot/index.js index.js
-rm -R DasitBot
-forever start index.js
+rm /var/dasit-bot/index.js
+git clone https://github.com/fededale95/DasitBot.git /var/dasit-bot/DasitBot
+mv /var/dasit-bot/DasitBot/index.js /var/dasit-bot/index.js
+rm -R /var/dasit-bot/DasitBot
+forever start /var/dasit-bot/index.js
 echo "Done!"
+forever list
