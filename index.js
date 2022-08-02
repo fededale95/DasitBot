b4d28bd9f42d12ed698b75c3df6cb700e3d8e0c6
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-08-02 17:53:16 +0200

    dafda

--- index.js
+++ index.js
@@ -216,13 +216,15 @@ function lastVersion(data,type){  //type: 0=CS,  1=Web,  2=Doc
           myArray.push(tempArray[tempArray.length-1]);
       }
 
-      var fsOrd = require('fs');
-      var streamOrd = fsOrd.createWriteStream(homeFolder+'ord.txt', {flags:'w'});
-      streamOrd.write("myARRAY: \n\n");
-      for(i in myArray){
-         streamOrd.write(myArray[i]+"\n");
+      if(type==0){
+         var fsOrd = require('fs');
+         var streamOrd = fsOrd.createWriteStream(homeFolder+'ord.txt', {flags:'w'});
+         streamOrd.write("myARRAY: \n\n");
+         for(i in myArray){
+            streamOrd.write(myArray[i]+"\n");
+         }
+         stream1.end();
       }
-      stream1.end();
 
       if(type==1){
          for(i in myArray){
