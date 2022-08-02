fc85bb005492074a427907027c994f542c061b1a
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-08-02 15:42:22 +0200

    adcsac

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ function myBubbleSort(items){
       for(i in items){
          streamOrd.write(items[i]+"\n");
       }
-      if(parseInt(items[0][0]) > parseInt(items[1][0])) {
+      if(parseInt(items[7][0]) > parseInt(items[1][0])) {
          streamOrd.write("Funziaaa:"+items[0][0]+"\n");
       }
       streamOrd.end();
