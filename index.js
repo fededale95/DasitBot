df55613f7413aca234761a05ab86f585232997fa
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-08-03 09:45:02 +0200

    adsaf

--- index.js
+++ index.js
@@ -97,9 +97,7 @@ function parseMessage( msg ){
             }
         } else if(msg.message.text=="/start"){
             sendMes(msg.message.chat.id,"Benvenuto nel Bot Dasit, clicca sul menu per scegliere un comando.\nClicca /abilitazione per richiedere i permessi per tutti i comandi.");
-            client
-                .sendMessage(msg.message.chat.id, 'Inserisci la password: ')
-                .getUpdates().promise().then(function (res) {
+            client.getUpdates().promise().then(function (res) {
    					  myLog(res,"logPwd.txt");
                     sendMes(msg.message.chat.id,"Abilitazione avvenuta correttamente!\n\nOra puoi utilizzare i seguenti comandi\n/dmsweb\n/dmsema\n/dmsdoctor\n/vpn");
    				 });
