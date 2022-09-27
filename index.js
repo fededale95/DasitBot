639174d4d131427b4993ed02d9354533241d911b
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-09-27 16:02:25 +0200

    dSAD

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function parseMessage( msg ){
                  var vpn_list="lista: \n\n";
 
                  for(i=0; i<files.length; i++){
-                    tmp = files[i]+"\n"
+                    tmp = files[i]+"\n";
                     vpn_list += tmp;
                  }
 
